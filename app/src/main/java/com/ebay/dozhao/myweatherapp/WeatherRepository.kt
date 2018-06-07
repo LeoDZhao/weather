bd9158dd88e38c36d71cd9c87bf3c4cc3c40fcd0
dozhao <dozhao@ebay.com>
2018-06-07 12:19:30 +0200

    add support for zip code

--- app/src/main/java/com/ebay/dozhao/myweatherapp/WeatherRepository.kt
+++ app/src/main/java/com/ebay/dozhao/myweatherapp/WeatherRepository.kt
@@ -8,24 +8,21 @@ object WeatherRepository {
     private var currentWeather: RawCurrentWeather? = null
 
     fun requestWeatherFromAPI(query: String) {
+        if (query.isEmpty()) return
         val retrofit = Retrofit.Builder()
                 .baseUrl("http://api.openweathermap.org/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
         val weatherService = retrofit.create(WeatherService::class.java)
-        val response = weatherService.currentWeatherByCitiyName(query).execute()
+        val response =
+                if (query.first().isLetter()) weatherService.currentWeatherByCitiyName(query).execute()
+                else weatherService.currentWeatherByZipCode(query).execute()
 
         val event = SearchDoneEvent()
         if (response.isSuccessful) {
             val body = response.body()
             body?.let {
                 currentWeather = it
-//                event.message = body.id.toString() + "\n" +
-//                        (body.name) + "\n" +
-//                        body.cod + "\n" +
-//                        body.coord.lon + ", " + body.coord.lat + "\n" +
-//                        body.weatherList.size + "\n" +
-//                        "${body.weatherList[0].id}, ${body.weatherList[0].main}, ${body.weatherList[0].description}, ${body.weatherList[0].icon}"
             }
 
         } else {
