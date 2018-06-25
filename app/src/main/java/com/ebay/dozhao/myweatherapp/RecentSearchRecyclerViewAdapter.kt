689931cc1a4d37f6e3b409b65aea891336e99d9d
dozhao <dozhao@ebay.com>
2018-06-25 13:35:26 +0800

    set click listener for recent search recycler view

--- app/src/main/java/com/ebay/dozhao/myweatherapp/RecentSearchRecyclerViewAdapter.kt
+++ app/src/main/java/com/ebay/dozhao/myweatherapp/RecentSearchRecyclerViewAdapter.kt
@@ -7,13 +7,14 @@ import android.view.ViewGroup
 import android.widget.TextView
 
 class RecentSearchRecyclerViewAdapter(private val recentSearches: ArrayList<String>)
-    : RecyclerView.Adapter<RecentSearchRecyclerViewAdapter.ViewHolder>() {
+    : RecyclerView.Adapter<RecentSearchRecyclerViewAdapter.ViewHolder>(), View.OnClickListener{
 
     class ViewHolder(val view: View) : RecyclerView.ViewHolder(view)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
                 .inflate(R.layout.recent_search_item, parent, false)
+        view.setOnClickListener(this)
         return ViewHolder(view)
     }
 
@@ -22,5 +23,14 @@ class RecentSearchRecyclerViewAdapter(private val recentSearches: ArrayList<Stri
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val textView = holder.view.findViewById<TextView>(R.id.recent_search_item_text)
         textView.text = recentSearches[position]
+    }
+
+    override fun onClick(view: View?) {
+        view?.let {
+            val query = it.findViewById<TextView>(R.id.recent_search_item_text).text.toString()
+            if (query.isNotEmpty()) {
+                NavigationUtils.startSearchResultActivity(view.context, query)
+            }
+        }
     }
 }
